15ab0cf5c2fabfee8a3093212325176dc21e2571
AceSolleza <113578060+AceSolleza@users.noreply.github.com>
2022-09-20 02:56:18 -1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class STRUCT {
+    struct Employee {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public string CompletedTraining;
+    }
+    public static void Main(string[] args){
+
+        Console.Title = "Struct";
+        Employee employee1;
+        employee1.FirstName = "Juan";
+        employee1.LastName = "DelaCruz";
+        employee1.Age = 25;
+        employee1.CompletedTraining = "Yes";
+
+        Console.WriteLine("Name:" + employee1.FirstName);
+        Console.WriteLine("LastName:" + employee1.LastName);
+        Console.WriteLine("Age" + employee1.Age);
+        Console.WriteLine("CompletedTraining" + employee1.CompletedTraining);
+        Console.ReadLine( );
+    }
+}
+
+
